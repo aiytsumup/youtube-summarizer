40aed71180cff649a436f39d14488cf4155541bb
aiytsumup <jinaljhawar55@gmail.com>
2026-07-21 00:06:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if st.button("Summarize Video"):
                 Please provide the summary exactly in the {language} language."""
                 
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                    model="gemini-3.5-flash",
                     contents=[prompt, transcript_text]
                 )
                 
